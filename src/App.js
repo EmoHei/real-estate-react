b668cc12791ba0fcd0bbeb1f34cafe62a52df528
EmoHei <98215816+EmoHei@users.noreply.github.com>
2023-01-08 20:30:41 +0100

    create components and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Offers from "./Pages/Offers";
+import Profile from "./Pages/Profile";
+import ForgotPassword from "./Pages/ForgotPassword";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
 function App() {
   return (
     <div >
-      <h1 className="text-2xl underline bg-red-400">Hello world</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/profile" element={<Profile></Profile>} />
+          <Route path="/sign-in" element={<SignIn></SignIn>} />
+          <Route path="/sign-up" element={<SignUp></SignUp>} />
+          <Route path="/forgot-password" element={<ForgotPassword></ForgotPassword>} />
+          <Route path="/offers" element={<Offers></Offers>} />
+        </Routes>
+      </Router>
     </div >
   );
 }
