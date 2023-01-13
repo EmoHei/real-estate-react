8d3f0622c97be3db40ce1be2fc1925eac823fff6
EmoHei <98215816+EmoHei@users.noreply.github.com>
2023-01-13 19:15:53 +0100

    create spinner component

---
+++ src/components/spinner/SpinnerComp.jsx
@@ -0,0 +1,12 @@
+import spinner from '../../assets/svg/spinner.svg'
+import React from 'react'
+import '../spinner/SpinnerComp.css';
+export default function SpinnerComp() {
+  return (
+    <div className='spinner-container'>
+      <div>
+        <img src={spinner }alt="Loading..." className='spinner' />
+      </div>
+    </div>
+  )
+}
