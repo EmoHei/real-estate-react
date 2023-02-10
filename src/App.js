43a6ffd64775e554a0c6b17e8570abfc94246da8
EmoHei <98215816+EmoHei@users.noreply.github.com>
2023-02-10 13:33:34 +0100

    # no changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import PrivateRoute from './components/PrivateRoute';
 import Home from "./pages/home/Home";
 import Offers from "./pages/offers/Offers";
